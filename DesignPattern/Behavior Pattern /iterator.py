9945998e1dad4ccef0097254438abf69d521141a
Xiaolan Li <xiaolan.carol.li@gmail.com>
2020-05-31 21:40:05 -0500

    added iterator pattern

---
+++ DesignPattern/Behavior Pattern /iterator.py
@@ -0,0 +1,11 @@
+def iterator(iterable, n):
+    iterator = zip(range(n), iterable)
+    for i, item in iterator:
+        # yield: continuously output items
+        yield item
+
+iterable = ['Apple', 'Orange', 'Pear', 'Fear']
+
+for i in iterator(iterable, 3):
+    print(i)
+
